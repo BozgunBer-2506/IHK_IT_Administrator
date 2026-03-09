4804febbd5d9b3b6d82decab7e688fa9aef9c7b5
mmarceldean-sketch <mmarceldean@gmail.com>
2026-03-09 17:29:48 +0100

    Struktur Ordnung + api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,19 @@
+import axios from "axios";
 
+const API = axios.create({
+    baseURL: "http://127.0.0.1:8000"
+});
+
+export const getMovies = () => API.get("/filme");
+
+export const addMovie = (movie) =>
+    API.post("/filme", movie);
+
+export const deleteMovie = (id) =>
+    API.delete(`/filme/${id}`);
+
+export const rateMovies = (id, rating) =>
+    API.post(`/filme/${id}/bewertung`, rating);
+
+export const getStats = () =>
+    API.get("/statistik");
