2d4f62cbf2eb57b85d45d6b5ffd894ecac440fbe
mmarceldean-sketch <mmarceldean@gmail.com>
2026-03-09 17:53:50 +0100

    APP.js erstellt, zeigt die Filmliste an

--- frontend/src/APP.js
+++ frontend/src/APP.js
@@ -1,1 +1,17 @@
+import React from "react";
+import MovieList from "./components/MovieList";
 
+function App() {
+
+    return (
+        <div>
+
+          <h1>Movie Bewertung App</h1>
+
+          <MovieList />
+        
+        </div>
+    );
+}
+
+export default App;
