94e01cb13c3e9543fef997bfc6a62c99186a7631
mmarceldean-sketch <mmarceldean@gmail.com>
2026-03-09 17:49:51 +0100

    MovieList.js erstellt, ruft das Backend auf und zeigt Filme an

--- frontend/src/components/MovieList.js
+++ frontend/src/components/MovieList.js
@@ -1,1 +1,34 @@
+import React, { useEffect, useState} from "react";
+import {getMovies} from "../services/api";
 
+function MovieList() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        loadMovies();
+    }, []);
+
+    const loadMovies = async () => {
+        const response = await getMovies();
+        setMovies(response.data)
+    };
+
+    return (
+      <div>
+
+        <h2>Filmliste</h2>
+
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <h3>{movie.title}</h3>
+            <p>Regisseur: {movie.regisseur}</p>
+            <p>Jahr: {movie.erscheinungsjahr}</p>
+            <p>Kategorie: {movie.kategorie}</p>
+          </div>
+        ))}
+
+        </div>
+    );
+}
+
+export default MovieList;
